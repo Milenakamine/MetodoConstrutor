31d16151d236a66a2e0db30ff33819574ddc2a38
Milenakamine <aakaminemilena@gmail.com>
2020-06-12 14:27:29 -0300

    Metodos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MetodoConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto roupa = new Produto(32);
+            roupa.Nome = "Dolce & Gabbana";
+
+            Produto sapato = new Produto (34);
+            System.Console.WriteLine(sapato.Codigo);
+
+
+            Produto sobretudo = new Produto(37, "Amaro", "Bege,", 326);
+            System.Console.WriteLine(sobretudo.Codigo);
+            System.Console.WriteLine(sobretudo.Nome);
+            System.Console.WriteLine(sobretudo.Descricao);
+            System.Console.WriteLine(sobretudo.Estoque);
+
+        }
+    }
+}
